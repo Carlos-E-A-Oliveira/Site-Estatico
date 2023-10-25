231776e14074f90c3db71c06f535f521b7cbdf3d
Carlos Eduardo A Oliveira <ceaoliveira7@gmail.com>
2023-10-25 20:11:00 -0300

    Add files via upload

---
+++ paginar.js
@@ -0,0 +1,15 @@
+function pagina1 ()
+{
+    window.location.href = "./Culinaria1.html"
+    
+}
+
+function pagina2 ()
+{
+    window.location.href = "./Culinaria2.html"
+}
+
+function pagina3 ()
+{
+    window.location.href = "./Culinaria3.html"
+}
